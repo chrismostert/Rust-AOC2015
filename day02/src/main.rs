9cdc2ed3c3a037f9acb0f3ea43f10bf31828607d
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-04-04 19:40:36 +0200

    Small cleanup

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -16,17 +16,18 @@ impl Present {
 }
 
 fn get_paper_amount(p: &Present) -> usize {
-    let res = [p.l * p.w, p.w * p.h, p.h * p.l]
+    let (area_sum, min_size) = [p.l * p.w, p.w * p.h, p.h * p.l]
         .iter()
         .fold((0, usize::MAX), |acc, &x| (acc.0 + 2 * x, x.min(acc.1)));
-    res.0 + res.1
+    area_sum + min_size
 }
 
 fn get_ribbon_amount(p: &Present) -> usize {
-    let mut s = vec![p.l, p.w, p.h];
+    let mut s = [p.l, p.w, p.h];
     s.sort_unstable();
-    2 * s[0] + 2 * s[1] + s[0] * s[1] * s[2]
+    2 * s[0] + 2 * s[1] + (s[0] * s[1] * s[2])
 }
+
 fn main() {
     let input: Vec<Present> = include_str!("../input.txt")
         .lines()
