dfef328d49e1475cb3281545deea78041b8e8f52
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-04-09 16:10:00 +0200

    Clean up day07

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,13 +1,14 @@
 use itertools::Itertools;
 use std::collections::HashMap;
 
-pub enum Instruction {
+enum Instruction {
     Set(&'static str),
     Not(&'static str),
     And(&'static str, &'static str),
     Or(&'static str, &'static str),
     Lshift(&'static str, &'static str),
     Rshift(&'static str, &'static str),
+    Setnum(u16),
 }
 
 fn parse_instruction(inst: &'static str) -> (Instruction, &str) {
@@ -32,51 +33,51 @@ fn execute_instruction(
     query: &str,
     cache: &mut HashMap<&str, u16>,
 ) -> u16 {
-    let mut exec_cache = |query| {
-        if let Some(&v) = cache.get(query) {
-            return v;
-        };
-        let res = execute_instruction(wires, query, cache);
-        cache.insert(query, res);
-        res
-    };
-
     if let Ok(v) = query.parse::<u16>() {
         return v;
     }
     match wires[query] {
-        Instruction::Set(a) => {
-            if let Ok(v) = a.parse() {
-                return v;
-            }
-            exec_cache(a)
-        }
-        Instruction::Not(a) => !(exec_cache(a)),
-        Instruction::And(a, b) => exec_cache(a) & exec_cache(b),
-        Instruction::Or(a, b) => exec_cache(a) | exec_cache(b),
-        Instruction::Lshift(a, n) => exec_cache(a) << n.parse::<usize>().unwrap(),
-        Instruction::Rshift(a, n) => exec_cache(a) >> n.parse::<usize>().unwrap(),
+        Instruction::Set(a) => exec_cache(wires, a, cache),
+        Instruction::Setnum(a) => a,
+        Instruction::Not(a) => !(exec_cache(wires, a, cache)),
+        Instruction::And(a, b) => exec_cache(wires, a, cache) & exec_cache(wires, b, cache),
+        Instruction::Or(a, b) => exec_cache(wires, a, cache) | exec_cache(wires, b, cache),
+        Instruction::Lshift(a, n) => exec_cache(wires, a, cache) << n.parse::<usize>().unwrap(),
+        Instruction::Rshift(a, n) => exec_cache(wires, a, cache) >> n.parse::<usize>().unwrap(),
     }
 }
 
-fn get_final_value(wires: &HashMap<&str, Instruction>, query: &str) -> u16 {
+fn exec_cache(
+    wires: &HashMap<&str, Instruction>,
+    query: &'static str,
+    cache: &mut HashMap<&str, u16>,
+) -> u16 {
+    if let Some(&v) = cache.get(query) {
+        return v;
+    };
+    let res = execute_instruction(wires, query, cache);
+    cache.insert(query, res);
+    res
+}
+
+fn find_value(wires: &HashMap<&str, Instruction>, query: &str) -> u16 {
     let mut cache: HashMap<&str, u16> = HashMap::new();
     execute_instruction(wires, query, &mut cache)
 }
 
 fn main() {
-    let input = include_str!("../input.txt").lines().collect_vec();
+    let mut wires = include_str!("../input.txt")
+        .lines()
+        .map(parse_instruction)
+        .fold(HashMap::new(), |mut acc, (inst, dest)| {
+            acc.insert(dest, inst);
+            acc
+        });
 
-    let mut wires =
-        input
-            .into_iter()
-            .map(parse_instruction)
-            .fold(HashMap::new(), |mut acc, (inst, dest)| {
-                acc.insert(dest, inst);
-                acc
-            });
+    let p1 = find_value(&wires, "a");
+    wires.insert("b", Instruction::Setnum(p1));
+    let p2 = find_value(&wires, "a");
 
-    println!("Part 1: {}", get_final_value(&wires, "a"));
-    wires.insert("b", Instruction::Set("956"));
-    println!("Part 2: {}", get_final_value(&wires, "a"));
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
 }
