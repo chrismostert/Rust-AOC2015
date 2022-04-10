9e0ba8e6a6c54989221ddaa6f4afe7c85111c1cb
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-04-10 21:18:06 +0200

    Day10

---
+++ day10/src/main.rs
@@ -0,0 +1,25 @@
+use itertools::Itertools;
+
+fn look_say(input: String) -> String {
+    input.chars().group_by(|c| *c).into_iter().fold(
+        String::with_capacity(2 * input.len()),
+        |mut acc, (key, group)| {
+            acc.push(char::from_digit(group.count() as u32, 10).unwrap());
+            acc.push(key);
+            acc
+        },
+    )
+}
+
+fn main() {
+    let mut look_say_str = String::from("1113122113");
+    for _ in 0..40 {
+        look_say_str = look_say(look_say_str)
+    }
+    println!("Part 1: {}", look_say_str.len());
+
+    for _ in 0..10 {
+        look_say_str = look_say(look_say_str)
+    }
+    println!("Part 2: {}", look_say_str.len());
+}
